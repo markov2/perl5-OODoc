d0428d10b7cc2a1ab209fefe06643fcea3ca8826
Mark Overmeer <mark@overmeer.net>
2025-08-15 14:54:34 +0200

    start next release

--- lib/OODoc.pm
+++ lib/OODoc.pm
@@ -9,7 +9,7 @@ use parent 'OODoc::Object';
 use strict;
 use warnings;
 
-our $VERSION = '3.02';  # needed here for own release process
+our $VERSION = '3.03';  # needed here for own release process
 
 use Log::Report    'oodoc';
 
